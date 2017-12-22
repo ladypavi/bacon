aeeeaa0b217477fbdffaa92d0ef39997db45a500
Unknown <pavielle.haines@gmail.com>
2017-12-22 00:21:13 -0500

    Init

---
+++ hello.py
@@ -0,0 +1,31 @@
+name = "Pavi"
+print("hello world")
+print("hello Pavi")
+#print("hello " + name)
+print("hello %s" % name)
+
+if name == "Pavi":
+    print("Yay!")
+else:
+    print("Boo!")
+
+groceries = ["Lettuce", "Tomato", "Bacon"]
+print(groceries)
+
+for i in groceries:
+    print(i)
+    if i == "Bacon":
+        print("Yummy yummy in my tummy!")
+
+groceries.append("Mayo")
+
+for i in groceries:
+    print(i)
+    if i == "Mayo":
+        print("Blech!")
+
+bad_mayo = groceries.index("Mayo")
+
+groceries[bad_mayo] = "Mustard"
+
+print(groceries)
